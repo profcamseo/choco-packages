86166957e86a317d3961d6dc67162a25bd2a58ac
profcamseo <53740347+profcamseo@users.noreply.github.com>
2023-07-16 19:27:26 +1000

    initial commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url        = 'https://download.anytype.io/?action=download&key=desktop&id=116988218'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe' 
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'anytype*' 
+
+  checksum      = 'CB4C9E815C495A03C0B96BC109FD70031F68F410EFCF9F6BF7DAE254ECDBD09E'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,5 @@
+﻿$ErrorActionPreference = 'Stop';
+$uninstDir = "C:\Users\Cameron\AppData\Local\Programs\anytype\Uninstall Anytype.exe"
+$uninstalled = $false
+
+Uninstall-ChocolateyPackage -PackageName 'anytype' -FileType 'EXE' -File $uninstDir

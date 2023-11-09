c1a5aab2121728ad62ee124a6302fe874a74bbbb
profcamseo <53740347+profcamseo@users.noreply.github.com>
2023-11-10 10:01:09 +1100

    screaming frog 19.4.0

--- screamingfrog/tools/chocolateyinstall.ps1
+++ screamingfrog/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url        = 'https://download.screamingfrog.co.uk/products/seo-spider/ScreamingFrogSEOSpider-19.3.exe?noredirect'
+$url        = 'https://download.screamingfrog.co.uk/products/seo-spider/ScreamingFrogSEOSpider-19.4.exe?noredirect'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -11,7 +11,7 @@ $packageArgs = @{
 
   softwareName  = 'screamingfrog*' 
 
-  checksum      = '7CAE9E1099098C7C58CC24682D5707F38C18AC69D4CDDFEBC06C2E4135A7E1D2'
+  checksum      = 'EA78BD72A908892B001EDC3327086CC41336EBC268DB27D49555B3BDA0DB250D'
   checksumType  = 'sha256' 
 
   silentArgs    = "ScreamingFrogSEOSpider-VERSION.exe /VERYSILENT"
